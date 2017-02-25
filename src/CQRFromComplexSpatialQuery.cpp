6ccbecb1af821af72f782c60c1ad3e8b64adc234
Daniel Bahrdt <daniel.git@funroll-loops.de>
2017-02-25 12:10:50 +0100

    clip size of compass polygons

--- src/CQRFromComplexSpatialQuery.cpp
+++ src/CQRFromComplexSpatialQuery.cpp
@@ -362,7 +362,7 @@ createPolygon(
 	liboscar::CQRFromComplexSpatialQuery::UnaryOp direction,
 	std::vector<sserialize::spatial::GeoPoint> & pp) const
 {
-	double len = way.length();
+	double len = std::max<double>(1000, way.length());
 	sserialize::spatial::GeoRect wayRect(way.boundary());
 	//determine a point that is northest, southest etc. as midpoint and then use the createPolygon from Point function
 	//to add the triangle in the view direction and then use the convex hull of this and the original way to create the final polygon
